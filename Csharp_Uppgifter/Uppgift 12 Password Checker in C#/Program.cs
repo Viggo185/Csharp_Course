9b2e59ef0bc0d9c3b4ccdeabbb066019327c2f38
Viggo185 <viggoantman@gmail.com>
2025-11-03 13:43:11 +0200

    adding new uppgifter

---
+++ Csharp_Uppgifter/Uppgift 12 Password Checker in C#/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_12_Password_Checker_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a password:");
+            string password1 = Console.ReadLine();
+
+            Console.WriteLine("Re-enter your password:");
+            string password2 = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(password1) || string.IsNullOrEmpty(password2))
+            {
+                Console.WriteLine("Pleas enter a password");
+            }
+            else if (password1.Length <6)
+            {
+                Console.WriteLine("Passwor must be 6 characters long");
+            }
+            else if (password1 == password2)
+            {
+                Console.WriteLine("Password match");
+            }
+            else
+            {
+                Console.WriteLine("Passwordss do not match");
+            }
+                
+        }
+    }
+}
---
+++ Csharp_Uppgifter/Uppgift 13  Odd or Even C#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_13__Odd_or_Even_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pleas enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine($"{num1} is even");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} is odd");
+            }
+        }
+    }
+}
